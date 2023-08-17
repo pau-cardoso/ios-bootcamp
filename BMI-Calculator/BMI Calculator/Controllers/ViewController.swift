ee83845b0a19011b275e53d9b1270f4ad4214914
pau-cardoso <paulinacardoso7@gmail.com>
2023-08-17 11:55:17 -0600

    add: BMI Calculator project app

---
+++ BMI-Calculator/BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2fm", sender.value)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.0fKg", sender.value)
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight / (height*height)
+        
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format: "%.2f", bmi)
+        
+        self.present(secondVC, animated: true, completion: nil)
+    }
+}
+
